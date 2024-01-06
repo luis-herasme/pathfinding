1290c1876f6dfb1c21793930fafc1f364fd32589
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-01-06 17:36:39 -0400

    Update world.ts

--- src/bodies/world.ts
+++ src/bodies/world.ts
@@ -1,9 +1,5 @@
-import { Box2D } from "../box";
-import { Obstacle } from "../cell-decomposition";
-import Render from "../render/render";
 import { Vector2 } from "../vector";
-import { BoxBody } from "./box";
-import { CircleBody } from "./circle";
+import { Obstacle } from "../cell-decomposition";
 
 export type Bounds = {
   minX: number;
@@ -16,7 +12,6 @@ export type Body = {
   position: Vector2;
   velocity: Vector2;
   update(): void;
-  render(renderer: Render): void;
   collideWithWorldBounds(worldBounds: Bounds): void;
 } & Obstacle;
 
@@ -51,54 +46,5 @@ export class World {
     for (let i = 0; i < this.bodies.length; i++) {
       this.bodies[i].collideWithWorldBounds(this.worldBounds);
     }
-  }
-
-  static createRandomWorld({
-    worldBounds,
-    numberOfBodies,
-    size,
-    velocity,
-  }: {
-    worldBounds: Bounds;
-    numberOfBodies: number;
-    size: number;
-    velocity: number;
-  }) {
-    const bodies: Body[] = [];
-    // bodies.push(
-    //   new CircleBody({
-    //     radius: 100,
-    //     position: new Vector2(100, 100),
-    //     velocity: new Vector2(0, 0),
-    //   })
-    // );
-    for (let i = 0; i < numberOfBodies; i++) {
-      const x = size + Math.random() * (worldBounds.maxX - 2 * size);
-      const y = size + Math.random() * (worldBounds.maxY - 2 * size);
-
-      if (Math.random() > 0.5) {
-        bodies.push(
-          new BoxBody({
-            velocity: Vector2.random(velocity),
-            box: new Box2D(
-              x,
-              y,
-              size / 2 + (Math.random() * size) / 2,
-              size / 2 + (Math.random() * size) / 2
-            ),
-          })
-        );
-      } else {
-        bodies.push(
-          new CircleBody({
-            velocity: Vector2.random(velocity),
-            radius: size / 2 + (Math.random() * size) / 2,
-            position: new Vector2(x, y),
-          })
-        );
-      }
-    }
-
-    return new World({ worldBounds, bodies });
   }
 }
